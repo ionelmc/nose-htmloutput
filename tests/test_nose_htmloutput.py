2ff79221236fc2970c49877970deafc522f79e48
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-06-07 17:23:14 +0300

    Fix bunch of bugs.

--- tests/test_nose_htmloutput.py
+++ tests/test_nose_htmloutput.py
@@ -4,9 +4,16 @@ import subprocess
 
 
 def test_sample():
-    subprocess.call(['coverage', 'run', 'tests/nosetests.py', '--with-html', '--html-file=sample.html', 'tests/test_sample.py'])
+    subprocess.call(['coverage', 'run', 'tests/nosetests.py', '--verbose', '--with-html', '--html-file=sample.html', 'tests/test_sample.py'])
     output = open('sample.html').read()
 
+    assert """<tr>
+                    <td>test_sample</td>
+                    <td>0</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>3</td>
+                </tr>""" in output
     assert """<tr>
                     <td>test_sample.MainTestCase</td>
                     <td>1</td>
@@ -15,28 +22,28 @@ def test_sample():
                     <td>2</td>
                 </tr>""" in output
     assert """<tr>
-                    <td>test_sample</td>
-                    <td>1</td>
+                    <td>test_sample.SecondTestCase</td>
                     <td>0</td>
-                    <td>1</td>
+                    <td>0</td>
+                    <td>2</td>
                     <td>2</td>
                 </tr>""" in output
     assert """<tr>
                 <td><strong>Total</strong></td>
-                <td>2</td>
-                <td>0</td>
-                <td>2</td>
+                <td>1</td>
+                <td>1</td>
                 <td>4</td>
+                <td>7</td>
             </tr>""" in output
-    assert "<h2>test_sample.MainTestCase (1 failures)</h2>" in output
+
+
+    assert "<h2>test_sample.MainTestCase (1 failures, 0 errors)</h2>" in output
     assert '<section id="test_sample.MainTestCase:test_b">' in output
     assert '<h3>test_b: <strong>' in output
-    assert '<h2>test_sample (1 failures)</h2>' in output
     assert '<section id="test_sample:test_b">' in output
     assert '<h3>test_b: <strong>' in output
-    assert '<h2>test_sample.MainTestCase (1 failures)</h2>' in output
     assert '<li><a class="success">test_a</a></li>' in output
     assert '<li><a class="failed" href="#test_sample.MainTestCase:test_b">test_b</a></li>' in output
-    assert '<h2>test_sample (1 failures)</h2>' in output
+    assert '<h2>test_sample (0 failures, 1 errors)</h2>' in output
     assert '<li><a class="success">test_a</a></li>' in output
     assert '<li><a class="failed" href="#test_sample:test_b">test_b</a></li>' in output
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,5 @@
 import unittest
+from nose.exc import SkipTest
 
 
 class MainTestCase(unittest.TestCase):
@@ -9,9 +10,20 @@ class MainTestCase(unittest.TestCase):
         self.assertTrue(0, "Some details")
 
 
+class SecondTestCase(unittest.TestCase):
+    def test_a(self):
+        self.assertTrue(1)
+
+    def test_b(self):
+        self.assertTrue(1)
+
+
 def test_a():
     assert 1
 
 
 def test_b():
     raise RuntimeError("Some other details")
+
+def test_c():
+    raise SkipTest('skipped')
